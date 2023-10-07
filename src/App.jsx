67fb2906d0d0aa8937fb190294988d736bdef9f0
Jesus Mendoza Martinez <jsmendoza1312@gmail.com>
2023-10-06 21:30:26 -0500

    experience add

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,55 @@
 import React, { useEffect } from "react";
 import About from "./components/about/About.jsx";
+import Experience from "./components/linetime/Experience.jsx";
 
 const App = () => {
   useEffect(() => {
     let moon = document.getElementById("moon");
-    let btn = document.getElementById("btn");
     let fuji = document.getElementById("fujiMe");
     let fujiMe = document.getElementById("fuji");
     let montan1 = document.getElementById("montan-1");
-    let sombra1 = document.getElementById("sombra1");
+    let montan2 = document.getElementById("montan-2");
 
-    window.addEventListener("scroll", function () {
-      let value = window.scrollY;
-      moon.style.top = 8 + value * -0.2 + "%";
-      moon.style.left = value * 2 + "px";
-      sombra1.style.left = value * 0.1 + "px";
-      sombra1.style.top = value * 1 + "px";
-      fuji.style.top = value * 0.25 + "px";
-      fuji.style.left = value * -0.2 + "px";
-      fujiMe.style.top = value * 0.25 + "px";
-      fujiMe.style.left = value * -0.1 + "px";
-      montan1.style.top = value * -0.1 + "px";
-    });
+    const handleScroll = () => {
+      const value = window.scrollY;
+
+      if (window.innerWidth < 768) {
+        //codigo para pantallas pequeñas
+        moon.style.top = 8 + value * -0.2 + "%";
+        moon.style.left = value * 2 + "px";
+        fuji.style.left = value * 0 + "px";
+        fuji.style.top = value * 0.1 + "px";
+        fujiMe.style.top = value * -0.1 + "px";
+        montan1.style.top = value * 0.1 + "px";
+        montan2.style.top = value * -0.04 + "px";
+      } else {
+        //codigo para pantallas grandes
+        moon.style.top = 8 + value * -0.2 + "%";
+        moon.style.left = value * 2 + "px";
+        fuji.style.top = value * 0.2 + "px";
+        fujiMe.style.left = value * 0.1 + "px";
+        fujiMe.style.top = value * 0.1 + "px";
+        montan2.style.top = value * 0 + "px";
+        montan1.style.top = value * -0.05 + "px";
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleScroll);
+
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleScroll);
+    };
   }, []); // El segundo argumento vacío [] asegura que se ejecute solo una vez después del montaje inicial
 
   return (
     <>
       <section>
-        <div class="paralax">
-          <div class="paralax-item p-item1"></div>
+        <div className="paralax">
+          <div className="paralax-item p-item1"></div>
         </div>
 
         <section>
@@ -43,13 +64,15 @@ const App = () => {
           <a href="index.html#about" id="btn">
             Explore
           </a>
-          <img src="/src/assets/sombra1.png" id="sombra1" />
+
           <img src="/src/assets/montan-1.png" id="montan-1" />
           <img src="/src/assets/montan2.png" id="montan-2" />
         </section>
       </section>
 
       <About />
+      {""}
+      <Experience />
     </>
   );
 };
