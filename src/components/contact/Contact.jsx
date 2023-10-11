0208c26623434628b5a5240f19468116c78b6f56
Jesus Mendoza Martinez <jsmendoza1312@gmail.com>
2023-10-11 00:39:15 -0500

    validation one part

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,52 @@
+import React, { useEffect, useState } from "react";
 import "./contact.css";
+import { RiEmotionUnhappyFill } from "react-icons/ri";
 
 const Contact = () => {
+  const [showUnhappyIcon, setShowUnhappyIcon] = useState(false);
+
+  useEffect(() => {
+    const formulario = document.getElementById("form");
+    const inputs = document.querySelectorAll("#form__id input");
+
+    const expresion = {
+      nombre: /^[a-zA-ZA-ÿ\s]{1,40}$/,
+      correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    };
+
+    const validarFormulario = (e) => {
+      if (e.target && e.target.classList) {
+        switch (e.target.name) {
+          case "nombre":
+            if (expresion.nombre.test(e.target.value)) {
+              document
+                .getElementById("group__nombre")
+                .classList.remove("form-group-incorrecto");
+              setShowUnhappyIcon(false);
+            } else {
+              document
+                .getElementById("group__nombre")
+                .classList.add("form-group-incorrecto");
+              setShowUnhappyIcon(true);
+            }
+            break;
+          case "email":
+            break;
+        }
+      }
+    };
+
+    if (formulario) {
+      inputs.forEach((input) => {
+        input.addEventListener("keyup", validarFormulario);
+        input.addEventListener("blur", validarFormulario);
+      });
+
+      formulario.addEventListener("submit", (e) => {
+        e.preventDefault();
+      });
+    }
+  }, []);
   return (
     <>
       <section id="form">
@@ -9,25 +55,77 @@ const Contact = () => {
             Contact me 📧
           </h2>
 
-          <div class="form-container">
+          <div className="form-container">
             <form
-              class="form"
               action="https://formsubmit.co/df6ca9cef3e73c5b0e6cab31a0e2b7ba"
               method="POST"
+              className="form"
+              id="form__id"
             >
-              <div class="form-group">
-                <label for="nombre">You're name:</label>
-                <input type="text" name="name" />
-                <label for="correo">You're email:</label>
-                <input type="text" name="email" />
+              <div className="form-group" id="group__nombre">
+                <label htmlFor="nombre" className="lbl_form">
+                  You're name:
+                </label>
+                <div className="form__input__div">
+                  <input
+                    type="text"
+                    name="nombre"
+                    className="form__input"
+                    id="nombre"
+                  />
+                  <div className="form__validation_icon">
+                    {showUnhappyIcon && <RiEmotionUnhappyFill />}
+                  </div>
+                </div>
+                <p className="form__input-error">
+                  Tu nombre no puede ser menos de 3 digitos.
+                </p>
               </div>
-              <div class="form-group">
-                <label for="textarea">How can I help you?</label>
-                <textarea name="comments" id="textarea" rows="10" cols="50">
-                  {" "}
-                </textarea>
+
+              <div className="form-group" id="group__email">
+                <label htmlFor="correo" className="lbl_form">
+                  You're email:
+                </label>
+                <div className="form__input__div">
+                  <input
+                    type="text"
+                    name="email"
+                    className="form__input"
+                    id="correo"
+                  />
+                  <div className="form__validation_icon">
+                    {showUnhappyIcon && <RiEmotionUnhappyFill />}
+                  </div>
+                </div>
+                <p className="form__input-error">Tu email no es correcto.</p>
               </div>
-              <input type="submit" class="form-submit-btn" value="Submit" />
+
+              <div className="form-group" id="textArea__group">
+                <label htmlFor="textarea" className="textArea">
+                  How can I help you?
+                </label>
+
+                <textarea
+                  name="comments"
+                  id="textarea"
+                  rows="10"
+                  cols="50"
+                  defaultValue=" "
+                />
+              </div>
+              <div className="formulario__grupo formulario__grupo-btn-enviar">
+                <input
+                  type="submit"
+                  className="form-submit-btn"
+                  value="Submit"
+                />
+                <p
+                  className="form__mensaje__success"
+                  id="form__mensaje__success"
+                >
+                  Se envio informacion!
+                </p>
+              </div>
             </form>
           </div>
         </div>
