83053be2b1aa9e4c0c71e556a7c432278bb4cbc0
Jesus Mendoza Martinez <jsmendoza1312@gmail.com>
2023-11-21 19:04:27 -0500

    update new project

--- src/components/proyect/Proyect.jsx
+++ src/components/proyect/Proyect.jsx
@@ -1,7 +1,7 @@
 import "./proyect.css";
 import imgEpm from "../../assets/projectEpm.webp";
 import imgJoly from "../../assets/pagJoly.webp";
-import imgMaria from "../../assets/pagMaria.webp";
+import imgDrink from "../../assets/api-drink.webp";
 
 const Proyect = () => {
   return (
@@ -16,6 +16,24 @@ const Proyect = () => {
           </p>
         </div>
         <div className="cardProject">
+          <div className="contenCards">
+            <img src={imgDrink} id="imgCards" alt="" />
+            <div className="overlay">
+              <h3>Project API Cocktails 🍷</h3>
+              <p>
+                In this project he interacted with a free API on cocktails,
+                which is still in progress.
+              </p>
+              <a
+                href="https://api-drink-js1312.netlify.app/"
+                target="_blank"
+                className="btnPages"
+              >
+                Visit site
+              </a>
+            </div>
+          </div>
+
           <div className="contenCards">
             <img src={imgEpm} id="imgCards" alt="" />
             <div className="overlay">
@@ -46,24 +64,6 @@ const Proyect = () => {
               </p>
               <a
                 href="https://joly-guacamole.netlify.app//"
-                target="_blank"
-                className="btnPages"
-              >
-                Visit site
-              </a>
-            </div>
-          </div>
-          <div className="contenCards">
-            <img src={imgMaria} id="imgCards" alt="" />
-            <div className="overlay">
-              <h3>Project maria suculentas 🌱</h3>
-              <p>
-                I developed this project as a presentation prototype using
-                technologies such as React, Vite, and Tailwind CSS. It is hosted
-                on Netlify.
-              </p>
-              <a
-                href="https://mary-suculenta.netlify.app/"
                 target="_blank"
                 className="btnPages"
               >
